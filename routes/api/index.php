c5d635874bbe42f00a6ea8f240b5ec12629fe4b8
Rangga Adi Pratama <ranggaadipratama96@yahoo.com>
2024-06-27 11:02:09 +0700

    hilangkan laravel sementara

--- routes/api/index.php
+++ routes/api/index.php
@@ -4,20 +4,20 @@ use App\Http\Controllers\MicroticController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'laravel'], function () {
-    Route::get('/interfaces/{id}', [MicroticController::class, 'interfaceList']);
-    Route::get('/interfaces/ethernet/{id}', [MicroticController::class, 'interfaceEthernetList']);
+// Route::group(['prefix' => 'laravel'], function () {
+// });
+Route::get('/interfaces/{id}', [MicroticController::class, 'interfaceList']);
+Route::get('/interfaces/ethernet/{id}', [MicroticController::class, 'interfaceEthernetList']);
 
-    Route::get('/dns/{id}', [MicroticController::class, 'dns']);
-    Route::get('/ip/dhcp-servers/{id}', [MicroticController::class, 'ipDhcpServerList']);
-    Route::get('/ip/hotspot/{id}', [MicroticController::class, 'hotspotList']);
-    Route::get('/routes/{id}', [MicroticController::class, 'routeList']);
-    Route::get('/identity/{id}', [MicroticController::class, 'indentity']);
+Route::get('/dns/{id}', [MicroticController::class, 'dns']);
+Route::get('/ip/dhcp-servers/{id}', [MicroticController::class, 'ipDhcpServerList']);
+Route::get('/ip/hotspot/{id}', [MicroticController::class, 'hotspotList']);
+Route::get('/routes/{id}', [MicroticController::class, 'routeList']);
+Route::get('/identity/{id}', [MicroticController::class, 'indentity']);
 
-    Route::get('/logs', [MicroticController::class, 'logList']);
+Route::get('/logs', [MicroticController::class, 'logList']);
 
-    Route::get('/top-host-name/{id}', [MicroticController::class, 'topHostName']);
-    Route::get('/top-sites/{id}', [MicroticController::class, 'topSites']);
+Route::get('/top-host-name/{id}', [MicroticController::class, 'topHostName']);
+Route::get('/top-sites/{id}', [MicroticController::class, 'topSites']);
 
-    Route::get('/system/resources/{id}', [MicroticController::class, 'systemResources']);
-});
+Route::get('/system/resources/{id}', [MicroticController::class, 'systemResources']);
